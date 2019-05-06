746f4ad1495a9908bb0b3a54c05307789dd6d129
Harshad Deo <subterranean.hominid@gmail.com>
2019-05-06 17:31:35 +0530

    add benchmarks

---
+++ bench/samplebench.cpp
@@ -0,0 +1,24 @@
+#include <benchmark/benchmark.h>
+#include "Generated.h"
+
+static void BM_Generated(benchmark::State& state){
+  double x = 1.4;
+  double yMin = 1.0;
+  double yMax = 2.0;
+  int cnt = 1000;
+  double incr = (yMax - yMin) / cnt;
+
+  for(auto _ : state){
+    double y = yMin;
+    simianquant::ext::GeneratedResult res(0, 0, 0);
+    while(y < yMax){
+      benchmark::DoNotOptimize(res = simianquant::ext::Generated::apply(x, y));
+      benchmark::ClobberMemory();
+      y += incr;
+    }
+  }
+}
+
+BENCHMARK(BM_Generated);
+
+BENCHMARK_MAIN();
